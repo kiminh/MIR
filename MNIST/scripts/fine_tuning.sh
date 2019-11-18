8609d960abbb7407b038cf4e7dfca7a2876e0919
kris-singh <krishnakantsingh.singh@suiit.ac.in>
2019-11-18 22:32:14 +0900

    Add: Scripts

---
+++ MNIST/scripts/fine_tuning.sh
@@ -0,0 +1,1 @@
+python ./src/solver_baseline.py --clean_run True --config_file ./configs/mnist-split/fine_tuning.yaml
---
+++ MNIST/scripts/prepare_permutation_dataset.sh
@@ -0,0 +1,1 @@
+python ./src/prepare_data.py --d_type permute --n_task 10 --save_file ./torch_data/mnist/permute/
