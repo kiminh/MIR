9b1ca5086278d034f966ea7d05720c090a4c70a9
kris-singh <krishnakant.singh@suiit.ac.in>
2019-11-20 01:43:43 +0900

    Fix: Model 2 hidden layers of size 400

--- MNIST/src/model.py
+++ MNIST/src/model.py
@@ -3,14 +3,23 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
 
 class Model(nn.Module):
     def __init__(self, input_size, hidden_size, out_size):
         super(Model, self).__init__()
         self.layer1 = nn.Linear(input_size, hidden_size)
-        self.layer2 = nn.Linear(hidden_size, out_size)
+        self.layer2 = nn.Linear(hidden_size, hidden_size)
+        self.layer3 = nn.Linear(hidden_size, out_size)
+        # init_weights(self.layer1)
+        # init_weights(self.layer2)
+        # init_weights(self.layer3)
 
     def forward(self, x):
         x = F.relu(self.layer1(x))
-        x = F.log_softmax(self.layer2(x), dim=1)
+        x = F.relu(self.layer2(x))
+        x = self.layer3(x)
         return x
